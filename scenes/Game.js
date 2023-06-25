adffbf3a3e0cb27928444849f2b1dc2ab93ec59e
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2023-06-25 14:27:22 +0000

    player created and jumping added

--- scenes/Game.js
+++ scenes/Game.js
@@ -7,9 +7,12 @@ export default class Game extends Phaser.Scene{
     preload(){
       this.load.image('background','assets/background/bg_layer1.png')
 
-    //   load the platform image
+      //   load the platform image
       this.load.image('platform','assets/Environment/ground_grass.png')
+
+      this.load.image('bunny-stand','assets/Player/bunny1_stand.png');
     }
+    player;
     create(){
       this.add.image(240,320,'background');
       
@@ -26,8 +29,28 @@ export default class Game extends Phaser.Scene{
 
         const body = platform.body;
         body.updateFromGameObject();
-        
-    }
+      }
+      // create a bunny sprite
+      this.player = this.physics.add.sprite(240,320,'bunny-stand')
+      .setScale(0.5);
 
+      // create a collider
+      this.physics.add.collider(platforms, this.player);
+
+      this.player.body.checkCollision.up = false;
+      this.player.body.checkCollision.left = false;
+      this.player.body.checkCollision.right = false;
+
+      this.cameras.main.startFollow(this.player);
+    }
+    update(){
+      // find out from Arcade physics if the player's physics body
+      // is touching something below it
+      const touchingDown = this.player.body.touching.down;
+      if(touchingDown){
+
+        // this make the bunny jump straight up
+        this.player.setVelocityY(-300);
+      }
     }
 }
