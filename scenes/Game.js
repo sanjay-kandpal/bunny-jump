a49af67a4d54a0dfa9d4bd780c93943506f3d10c
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2023-06-25 15:10:06 +0000

    movement dead camera logic added

--- scenes/Game.js
+++ scenes/Game.js
@@ -1,10 +1,12 @@
 import Phaser from '../lib/phaser.js';
 
 export default class Game extends Phaser.Scene{
-    constructor(){
+  cursors  
+  constructor(){
         super('game');
-    }
+  }
     preload(){
+      this.cursors = this.input.keyboard.createCursorKeys();
       this.load.image('background','assets/background/bg_layer1.png')
 
       //   load the platform image
@@ -12,19 +14,23 @@ export default class Game extends Phaser.Scene{
 
       this.load.image('bunny-stand','assets/Player/bunny1_stand.png');
     }
-    player;
+  
+    platforms
+    player
     create(){
+      this.add.image(240,320,'background')
+      .setScrollFactor(1,0);
       this.add.image(240,320,'background');
       
      //   create the group
-     const platforms = this.physics.add.staticGroup();
+     this.platforms = this.physics.add.staticGroup();
 
      //  then create 5 platforms from the group
      for(let i = 0; i < 5 ; i++){
         const x = Phaser.Math.Between(80,400);
         const y = 150 * i;
         
-        const platform = platforms.create(x,y,'platform');
+        const platform = this.platforms.create(x,y,'platform');
         platform.scale = 0.5;
 
         const body = platform.body;
@@ -35,15 +41,31 @@ export default class Game extends Phaser.Scene{
       .setScale(0.5);
 
       // create a collider
-      this.physics.add.collider(platforms, this.player);
+      this.physics.add.collider(this.platforms, this.player);
 
       this.player.body.checkCollision.up = false;
       this.player.body.checkCollision.left = false;
       this.player.body.checkCollision.right = false;
 
       this.cameras.main.startFollow(this.player);
+
+      this.cameras.main.startFollow(this.player);
+
+      // set the horizontal dead zone 1.5x game width
+      this.cameras.main.setDeadzone(this.scale.width * 1.5);
     }
-    update(){
+    update(t,dt){
+
+      this.platforms.children.iterate(child => {
+        const platform = child;
+        const scrollY = this.cameras.main.scrollY;
+
+        if(platform.y >= scrollY + 700){
+          platform.y = scrollY - Phaser.Math.Between(50,100);
+          platform.body.updateFromGameObject()
+        }
+      })
+    
       // find out from Arcade physics if the player's physics body
       // is touching something below it
       const touchingDown = this.player.body.touching.down;
@@ -51,6 +73,14 @@ export default class Game extends Phaser.Scene{
 
         // this make the bunny jump straight up
         this.player.setVelocityY(-300);
+      }
+      if(this.cursors.left.isDown && !touchingDown){
+        this.player.setVelocityX(-200);
+      }else if(this.cursors.right.isDown && !touchingDown){
+        this.player.setVelocityX(200);
+      }else{
+        // stop movement if not left or right
+        this.player.setVelocityX(0);
       }
     }
 }
