617d29c81be06eb457b5144df426dc203fd47a9c
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2023-06-25 06:30:29 +0000

    physics added

--- scenes/Game.js
+++ scenes/Game.js
@@ -12,7 +12,22 @@ export default class Game extends Phaser.Scene{
     }
     create(){
       this.add.image(240,320,'background');
-      this.add.image(240,320,'platform')
-      .setScale(0.5);
+      
+     //   create the group
+     const platforms = this.physics.add.staticGroup();
+
+     //  then create 5 platforms from the group
+     for(let i = 0; i < 5 ; i++){
+        const x = Phaser.Math.Between(80,400);
+        const y = 150 * i;
+        
+        const platform = platforms.create(x,y,'platform');
+        platform.scale = 0.5;
+
+        const body = platform.body;
+        body.updateFromGameObject();
+        
+    }
+
     }
 }
